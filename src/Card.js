b8cb8e451e52157eb5c4e7450e97c6697e9da3c4
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-05-18 21:09:11 +0500

    i'm deleted inde.js file

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Card extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          <i>i think work</i>
+        </h1>
+      </div>
+    );
+  }
+}
